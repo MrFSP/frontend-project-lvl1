04d132348ea1c11a2d8161b21d73d6e19ae86795
Pavel Egorov <epe.m@mail.ru>
2021-11-14 01:12:13 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,26 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const roundsCount = 3;
+
+export default (task, getQuestion) => {
   console.log('\nWelcome to the Brain Games!');
-  const userName = readlineSync.question('\nMay I have your name? ');
+  console.log(task || '');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
+  if (!task) {
+    return;
+  }
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const { question, answer } = getQuestion();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === answer) {
+      console.log('Correct!\n');
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".`);
+      console.log(`\nLet's try again, ${userName}!\n`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!\n`);
 };
