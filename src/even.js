8645e842a06e57f31fbd81f3319f970eec1bf981
Pavel Egorov <epe.m@mail.ru>
2021-11-14 01:10:19 +0300

    Add even.js

---
+++ src/even.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/extensions */
+
+import { getRandomInt } from './utils/functions.js';
+
+import game from './cli.js';
+
+const maxNumber = 100;
+
+const task = 'Answer "yes" if number even otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+
+const getData = () => {
+  const question = getRandomInt(0, maxNumber);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return { question, answer };
+};
+
+export default () => game(task, getData);
